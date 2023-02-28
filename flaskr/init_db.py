25376815ab676f098551feb0143923dc081d1a32
stephen <lee.stephen@courrier.uqam.ca>
2023-02-28 18:33:51 -0500

    d

---
+++ flaskr/init_db.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+connection = sqlite3.connect('articles.db')
+
+with open('flaskr/schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO article (id, title, identifiant, auteur, date_publication, paragraphe) VALUES (?, ?, ?, ?, ?, ?)",
+            (1,'First Post', 'Content for the first post', 'Alice Shmirtz', '2018-05-29', 'Il etait une fois Alice')
+            )
+
+
+connection.commit()
+connection.close()
