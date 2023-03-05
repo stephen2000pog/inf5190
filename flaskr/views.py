8e08422e76d452402a4503ae1af27d8e18520501
stephen <lee.stephen@courrier.uqam.ca>
2023-03-04 22:08:19 -0500

    push

--- flaskr/views.py
+++ flaskr/views.py
@@ -7,7 +7,7 @@ views = Blueprint('views', __name__)
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        g._database = Database() 
+        g._database = Database()
     return g._database
 
 
@@ -15,5 +15,13 @@ def get_db():
 def index():
     articles = get_db().get_article()
     return render_template("home.html", articles = articles)
+
+@views.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        query = request.form['query']
+        articles = get_db().search_articles(query)
+        return render_template("search.html", articles=articles, query=query)
+    return render_template("search.html")
 
 #@views.route('/article/<identifiant>')
