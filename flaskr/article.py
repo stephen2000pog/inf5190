9c7dc872c287c3d068e58083282e98314e7c0be5
stephen <lee.stephen@courrier.uqam.ca>
2023-03-05 18:56:19 -0500

    ajout de formulaire pour modifier les articles

--- flaskr/article.py
+++ flaskr/article.py
@@ -4,17 +4,18 @@ class Article(Database):
     def __init__(self):
         self.connection = None
 
-    def get_articles(self, number):
+    def get_articles_of_the_day(self, number):
         cursor = self.get_connection_row().cursor()
-        cursor.execute("SELECT * FROM article")
-        #"WHERE date_publication < date('now') "
-        #"ORDER BY date_publication DESC")
+        cursor.execute("SELECT * FROM article "
+        "WHERE date(date_publication) = date('now') "
+        "ORDER BY date_publication DESC")
         articles = cursor.fetchmany(number)
         return articles
     
     def get_article_by_id(self, identifiant):
         cursor = self.get_connection_row().cursor()
-        cursor.execute("SELECT * FROM article WHERE identifiant = ?", (identifiant,))
+        cursor.execute("SELECT * FROM article "
+        "WHERE identifiant = ?", (identifiant,))
         article = cursor.fetchone()
         return article
     
@@ -28,14 +29,22 @@ class Article(Database):
     def insert_article(self, titre, identifiant, auteur, date, paragraphe):
         connection = self.get_connection_row()
         cursor = connection.cursor()
-        cursor.execute(("insert into article(titre, identifiant, auteur, date_publication, paragraphe) "
+        cursor.execute(("INSERT into article(titre, identifiant, auteur, date_publication, paragraphe) "
                         "values(?, ?, ?, ?, ?)"), (titre, identifiant, auteur, date, paragraphe))
         connection.commit()
 
     def search_articles(self, query):
         connection = self.get_connection_row()
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM article WHERE titre LIKE ? OR paragraphe LIKE ?", ('%'+query+'%', '%'+query+'%'))
+        cursor.execute("SELECT * FROM article "
+                       "WHERE titre LIKE ? OR paragraphe LIKE ?", ('%'+query+'%', '%'+query+'%'))
         articles = cursor.fetchall()
         return articles
+    
+    def update_article(self, identifiant, titre, paragraphe):
+        connection = self.get_connection()
+        cursor = connection.cursor()
+        cursor.execute("UPDATE article SET titre=?, paragraphe=? "
+                       "WHERE identifiant=?", (titre, paragraphe, identifiant))
+        connection.commit()
     
--- flaskr/views.py
+++ flaskr/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, g, request
+from flask import Blueprint, render_template, g, request, redirect
 from .article import Article
 
 views = Blueprint('views', __name__)
@@ -11,27 +11,19 @@ def get_db():
 
 @views.route('/')
 def home():
-    articles = get_db().get_articles(5)
+    articles = get_db().get_articles_of_the_day(5)
     return render_template("home.html", articles = articles)
+
+@views.route('/search')
+def search():
+    query = request.args.get('query')
+    articles = get_db().search_articles(query)
+    return render_template("search.html", articles=articles, query=query)
 
 @views.route('/admin')
 def admin_page():
     articles = get_db().get_all_articles()
     return render_template('admin.html', articles=articles)
-
-@views.route('/admin-modifier/<identifiant>')
-def admin_modifier(identifiant):
-    article = get_db().get_article_by_id(identifiant)
-    return render_template('admin-modifier.html', article=article)
-
-@views.route('/search', methods=['GET', 'POST'])
-def search():
-    if request.method == 'POST':
-        query = request.form['query']
-    elif request.method == 'GET':
-        query = request.args.get('query')
-    articles = get_db().search_articles(query)
-    return render_template("search.html", articles=articles, query=query)
 
 @views.route('/article/<identifiant>')
 def show_article(identifiant):
@@ -39,4 +31,27 @@ def show_article(identifiant):
     if article is None:
         return render_template('404.html'), 404
     return render_template('article.html', article=article)
+
+@views.route('/admin-modifier/<identifiant>')
+def admin_modifier(identifiant):
+    article = get_db().get_article_by_id(identifiant)
+    return render_template('admin-modifier.html', article=article)
+
+@views.route('/admin-post', methods=['POST'])
+def admin_post():
+    identifiant = request.form['identifiant']
+    titre = request.form['titre']
+    paragraphe = request.form['paragraphe']
+    get_db().update_article(identifiant, titre, paragraphe)
+    return redirect('/admin')
+
+@views.route('/admin-nouveau')
+def admin_nouveau():
+    return render_template('admin-nouveau.html')
+
+@views.route('/creation-article', methods=['POST'])
+def create_article():
+    return 
+
+
 
