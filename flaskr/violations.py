db93c9c08d5c025897ff6406bc1e258f58aa303e
stephen <lee.stephen@courrier.uqam.ca>
2023-04-09 23:52:03 -0400

    le critere de recherche fonctionne maintenant avec plusieurs mots

--- flaskr/violations.py
+++ flaskr/violations.py
@@ -7,7 +7,27 @@ class Violation(Database):
     def search_contravenants(self, query):
         connection = self.get_connection_row()
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM violations "
-                       "WHERE etablissement LIKE ? OR proprietaire LIKE ? OR adresse LIKE ?", ('%'+query+'%', '%'+query+'%', '%'+query+'%'))
-        contravenants = cursor.fetchall()       
+        search_terms = query.split()
+        sql_query = "SELECT * FROM violations WHERE "
+        params = []
+        for i, term in enumerate(search_terms):
+            if i > 0:
+                sql_query += " OR "
+            sql_query += "etablissement LIKE ? OR proprietaire LIKE ? OR adresse LIKE ?"
+            pattern = f"%{term}%"
+            params.extend([pattern, pattern, pattern])
+        cursor.execute(sql_query, params)
+        contravenants = cursor.fetchall()
+        connection.close()
         return contravenants
+    
+    def search_contraventions(self, du, au):
+        connection = self.get_connection_row()
+        cursor = connection.cursor()
+        query = "SELECT * FROM violations where strftime('%s', date) BETWEEN strftime('%s', ?) AND strftime('%s', ?)"
+        cursor.execute(query, (du, au))
+        contraventions = cursor.fetchall()
+        connection.close()
+        return contraventions
+    
+    
