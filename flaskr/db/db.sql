d48023fd03f9e60c0f636ad5e60ebf4dfb081cf8
stephen <lee.stephen@courrier.uqam.ca>
2023-04-09 23:51:39 -0400

    changement de type dans la bd + ajout de donnees dans le resultat de recherche

--- flaskr/db/db.sql
+++ flaskr/db/db.sql
@@ -1,15 +1,15 @@
 CREATE TABLE violations (
     id_poursuite INTEGER,
     business_id INTEGER,
-    date DATETIME,
+    date TEXT,
     description TEXT,
     adresse TEXT,
-    date_jugement DATETIME,
+    date_jugement TEXT,
     etablissement TEXT,
-    montant NUMERIC,
+    montant TEXT,
     proprietaire TEXT,
     ville TEXT,
     statut TEXT,
-    date_statut DATETIME,
+    date_statut TEXT,
     categorie TEXT
 );
