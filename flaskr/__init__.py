9131d590cbbb36a82cd67d8bd13fcd489d965e4d
stephen <lee.stephen@courrier.uqam.ca>
2023-04-14 23:26:40 -0400

    adding header in the url to dl excel file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,9 +6,9 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dqwrgepoads90s2j'
 
-    #scheduler = BackgroundScheduler(daemon=True)
-    #scheduler.add_job(func=insert_data, trigger="cron", hour=0, minute=0)
-    #scheduler.start()
+    scheduler = BackgroundScheduler(daemon=True)
+    scheduler.add_job(func=insert_data, trigger="cron", hour=0, minute=0)
+    scheduler.start()
 
     from .views import views
     app.register_blueprint(views, url_prefix='/')
--- flaskr/insert_data.py
+++ flaskr/insert_data.py
@@ -4,42 +4,32 @@
 import csv
 import sqlite3
 import urllib.request
-from urllib.request import Request, urlopen
-import logging
-#logging.basicConfig(filename='flaskr/logs/app.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 def insert_data():
-    # Téléchargement du fichier CSV
     url = "https://data.montreal.ca/dataset/05a9e718-6810-4e73-8bb9-5955efeb91a0/resource/7f939a08-be8a-45e1-b208-d8744dca8fc6/download/violations.csv"
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-        'Accept-Encoding': 'none',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'Connection': 'keep-alive'}
-    req = Request(url, headers=headers)
+    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+    headers={'User-Agent':user_agent,} 
     filename = "flaskr/violations.csv"
-    urllib.request.urlretrieve(req, filename)
-    #urlopen(req).read(filename)
-        # Connexion à la base de données SQLite
+    request=urllib.request.Request(url,None,headers)
+    response = urllib.request.urlopen(request)
+    data = response.read() 
+
+    with open(filename, 'wb') as f:
+        f.write(data)
+
     connection = sqlite3.connect("flaskr/db/violations.db")
     cursor = connection.cursor()
 
-        # Suppression de toutes les données existantes
-        #cursor.execute("DELETE FROM violations")
-
-        # Lecture du fichier CSV et insertion des données dans la base de données
     with open(filename, "r", encoding="utf-8") as f:
         reader = csv.reader(f)
-        next(reader)  # Ignorer la première ligne d'en-tête
+        next(reader)  
         for row in reader:
             cursor.execute("""
                 INSERT INTO violations (id_poursuite, business_id, date, description, adresse, date_jugement, etablissement, montant, proprietaire, ville, statut, date_statut, categorie)
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                """, row)
+            """, row)
 
-        # Validation des changements et fermeture de la connexion
     connection.commit()
     connection.close()
 
-        #logging.info("Mise à jour de la base de donnée")
+insert_data()
